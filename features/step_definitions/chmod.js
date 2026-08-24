2074e1867592fb92b53d50a7b5205218cda0a61d
sergey324234 <ivan.m3prosto@mail.ru>
2026-08-25 04:11:53 +0800

    commit2

--- features/step_definitions/chmod.js
+++ features/step_definitions/chmod.js
@@ -1,9 +1,12 @@
+
+
 const {Given, When, Then, Before, After} = require('@cucumber/cucumber');
 
 
 const assert = require('assert');
 const fs = require('fs')
-const { promisify} = require('util');
+const promisify = require('util').promisify;
+
 
 const writeFileAsync = promisify(fs.writeFile);
 const unlinkAsync = promisify(fs.unlink);
@@ -11,37 +14,65 @@ const exec = require('child_process').exec;
 const execPromise = promisify(exec);
 
 
+const chalk = require('chalk');
 
 
 
+Given('существует объект {string} с правами {string}', async function(nameobj, mode) {
+    this.targetObj = nameobj;
+    this.oldMode = mode;
 
-Given('существует файл {string} с правами {int}', async function(filename, mode) {
-    this.targetFile = filename;
-
-    await writeFileAsync(filename, '213');
-    await execPromise(`chmod ${mode.toString(8)} ${filename}`);
+    await writeFileAsync(this.targetObj, '123');
+    await execPromise(`chmod ${this.oldMode} ${this.targetObj}`);
 });
 
-When('выполняется команда {string}', async function(command) {
-    try {
-        const {stdout, stderr} = await execPromise(command, {shell: true})
-        this.result = {stdout, stderr, code:0};
+When('я меняю права этого файла на {string}', async function(mode) {
 
+    this.newMode = mode;
+    try {
+        const {stdout, stderr} = await execPromise(`chmod ${this.newMode} ${this.targetObj}`);
+
+        this.result = {stdout, stderr, error: null} 
     }
-    catch (e) {
-        this.result = {
-            stdout: error.stdout || '',
-            stderr: error.stderr || '', 
-            code: error.code || 1,
-        };
+    catch(e) {
+        this.result = { error: e.message };
     }
+    
 })
 
-Then('код возврата должен быть {int}', function (expectedCode) {
-    assert.strictEqual(this.result.code, expectedCode);
+
+
+Then('сравниваем результат с {string}', async function (answerMode) {
+
+    if (answerMode === "error") {
+        if (this.result.error !== null) {
+            console.log(chalk.green(`\n[Ok] предвиденная ошибка:\n${this.result.error}`));
+            return;
+        }
+        throw new Error(`Ожидалась ошибка при chmod ${this.newMode}, но команда прошла успешно.`);
+    }
+
+    const stats = fs.statSync(this.targetObj);
+    const actualMode = (stats.mode & 0o7777).toString(8);
+
+    const expectedMode = parseInt(answerMode, 8).toString(8);
+
+    if(actualMode === expectedMode) {
+        console.log(chalk.green(`\n[Ok] Права совпадают!`))
+        return;
+    }
+
+    throw new Error(`Текущие права (${actualMode}) не совпадают с ожидаемыми (${expectedMode})`);
+
 });
+
 
 
 After(async function () {
-    await unlinkAsync(this.targetFile);
+
+    if (this.targetObj && fs.existsSync(this.targetObj)) {
+        await unlinkAsync(this.targetObj);
+    }
 })
+
+
