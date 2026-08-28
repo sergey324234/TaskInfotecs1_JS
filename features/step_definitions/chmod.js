3dc44e090232200be37c828a0f05b264742c6dde
sergey324234 <ivan.m3prosto@mail.ru>
2026-08-29 01:52:05 +0800

    commit7

--- features/step_definitions/chmod.js
+++ features/step_definitions/chmod.js
@@ -8,34 +8,59 @@ import fs from 'fs/promises'
 import path from 'path'
 
 
-import os from 'os'
-
 
 
 Before(async function (scenario) {
-    
     this.scenarioName = scenario.pickle.name;
-    this.mode = ""
 
-    this.targetFile = path.join(os.tmpdir(), `test.txt`);
-    await Bun.write(this.targetFile, '123');
+    this.newMode = "";
+    this.option = "";
+
 })
 
 Given('существует файл с правами {string}', async function(mode) {
 
-    await $`chmod ${mode} ${this.targetFile}`;
+    this.targetObj = path.join(process.cwd(), `test.txt`);
+    await Bun.write(this.targetObj, '123');
+
+    //this.targetObj = path.join(rootDir, 'FileTest.txt');
+    await $`chmod ${mode} ${this.targetObj}`;
+    
 })
 
-When("установлен символьный режим {string}", async function(SymbolMode) {
+Given('существует кластер файлов и папок с правами {string}', async function(mode) {
 
-    this.mode += SymbolMode;
+    this.targetObj = path.join(process.cwd(), 'DirTest');
+
+    const rootFile = path.join(this.targetObj, 'FileTest.txt');
+    const dir1 = path.join(this.targetObj, 'DirTest1');
+    const file2 = path.join(this.targetObj, 'DirTest2', 'FileTest2.txt');
+
+
+    await fs.mkdir(dir1, { recursive: true });
+    await Bun.write(rootFile, '123');
+    await Bun.write(file2, '123');
+
+    await $`chmod -R ${mode} ${this.targetObj}`;
+})
+
+
+When("установлена опция {string}", async function(option) {
+    this.option = option;
+})
+
+When("установлен символьный режим {string}", async function(symbolMode) {
+
+    this.newMode += symbolMode;
     
 })
 
 When("я меняю права этого объекта на {string}", async function(mode) {
 
-    this.mode += mode;
-    const proc = await $`chmod ${this.mode} ${this.targetFile}`.quiet().nothrow();
+    this.newMode += mode;
+    const args = [this.option, this.newMode, this.targetObj].filter(Boolean);
+
+    const proc = await $`chmod ${args}`.quiet().nothrow();
 
     this.result = {
         error: proc.exitCode ? proc.stderr.toString() : null
@@ -44,6 +69,15 @@ When("я меняю права этого объекта на {string}", async f
 })
 
 
+async function ExpectMode(obj ,answerMode) {
+
+    const stats = await fs.stat(obj);
+    const actualMode = (stats.mode & 0o7777).toString(8);
+
+    const expectedMode = parseInt(answerMode, 8).toString(8);
+    
+    expect(actualMode).toBe(expectedMode);
+}
 
 Then('сравниваем результат с {string}', async function (answerMode) {
 
@@ -58,22 +92,29 @@ Then('сравниваем результат с {string}', async function (answ
         throw new Error(`[${this.scenarioName}] непредвиденная ошибка:\n${this.result.error}`);
     }
 
-    const stats = await fs.stat(this.targetFile);
-    const actualMode = (stats.mode & 0o7777).toString(8);
-
-    const expectedMode = parseInt(answerMode, 8).toString(8);
-    
-    expect(actualMode).toBe(expectedMode);
+    await ExpectMode(this.targetObj, answerMode);
 
 });
+
+Then('проверяем кластер файлов на наличие новых прав', async function() {
+    const glob = new Bun.Glob("**/*");
+
+
+    for (const file of glob.scanSync({ cwd: this.targetObj , onlyFiles: false })) {
+        
+        const fullPath = path.join(this.targetObj, file);
+        await ExpectMode(fullPath, this.newMode);
+    }
+
+
+})
 
 
 
 After(async function () {
-
-
-    await fs.chmod(this.targetFile, 0o777);
-    await fs.unlink(this.targetFile);
+    
+    await fs.chmod(this.targetObj, 0o777);
+    await fs.rm(this.targetObj, { recursive: true, force: true });
     
 })
 
